4470297c68e6dfcdeff91990f813a8a71d4c2d2e
Elizabeth Jimenez <lizliz123@gmail.com>
2018-05-12 04:56:44 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,4 +13,10 @@ end
 
 
 def valid_move?(board, index)
-  
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
